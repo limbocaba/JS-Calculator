857a66bb6f46565f926fe27b3f9ab7b0b4d4dcbb
Sandy Caba <joriel@Sandys-MacBook-Air.local>
2022-10-21 10:01:10 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let add = (num1, num2) => {
+  let sum = num1 + num2
+  return sum
+}
+
+let subtract = (num1, num2) => {
+  let sum = num1 - num2
+  return sum
+}
+
+let multiply = (num1, num2) => {
+  let product = num1 * num2
+  return product
+}
+
+let divide = (num1, num2) => {
+  let quotient = num1 / num2 
+  return quotient
+}
+
+let exponent = (num1, num2) => {
+  let product = num1 ** num2
+  return product
+}
